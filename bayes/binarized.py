b67431d2d116575e4e26c4d715f21aceb3223dad
Tomos <tn90ca@gmail.com>
2015-06-24 12:41:33 -0400

    [FIX] Arranged Directories for Scripts
    Created separate directories for the classifier and twitter scripts.

---
+++ bayes/binarized.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# binarized.py
+# nb_twitter
+#
+# Created by Thomas Nelson <tn90ca@gmail.com>
+#            Preston Engstrom <>
+# Created..........................2015-06-24
+# Modified.........................2015-06-24
+#
+# This script was developed for use as part of the nb_twitter package
+
+
+import math
+import bayes
