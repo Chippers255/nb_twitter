4e3c6f20c15ac89ac7838c720e11d98c34a7d8d6
Tomos <tn90ca@gmail.com>
2015-06-30 10:35:19 -0400

    [ADD] Testing script for Binarized Class

---
+++ nb_twitter/test/test_binarized.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+# test_binarized.py
+# nb_twitter/nb_twitter/bayes
+#
+# Created by Thomas Nelson <tn90ca@gmail.com>
+#            Preston Engstrom <pe12nh@brocku.ca>
+# Created..........................2015-06-30
+# Modified.........................2015-06-30
+#
+# This script was developed for use as part of the nb_twitter package
+
+
+from nb_twitter.bayes import binarized
+
+
+train_class = ['c', 'j']
+
+train_docs = [['c', 'chinese beijing chinese'],
+              ['c', 'chinese chinese shanghai'],
+              ['c', 'chinese macao'],
+              ['j', 'tokyo japan chinese']]
+
+test_docs = 'chinese chinese chinese tokyo japan'
+
+classifier = binarized.Binarized(train_class, train_docs)
+
+classifier.train()
+results = classifier.run(test_docs)
+
+print("C\t\t=", classifier.C)
+print("D\t\t=", classifier.D)
+print("N\t\t=", classifier.N)
+print("V\t\t=", classifier.V)
+print("Nc\t\t=", classifier.Nc)
+print("Prior\t=", classifier.prior)
+print("Prob\t=", classifier.prob)
+print()
+print("J =", results['j'])
+print("C =", results['c'])
+print("Max =", max([results['j'], results['c']]))
